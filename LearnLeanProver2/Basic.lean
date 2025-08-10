fda84a41ede3e60194c67e6356cd0091bc96a89c
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-08-10 16:07:53 +0900

    wip: wip

--- LearnLeanProver2/Basic.lean
+++ LearnLeanProver2/Basic.lean
@@ -1,1 +1,5 @@
+import LearnLeanProver2.Sub
+
 def hello := "world"
+
+def plus_2 (n : Nat) : Nat := plus_1 (plus_1 n)
---
+++ LearnLeanProver2/Sub.lean
@@ -0,0 +1,3 @@
+theorem one_plus_two_eq_three : 1 + 2 = 3 := by rfl
+
+def plus_1 (n : Nat) : Nat := n + 1
