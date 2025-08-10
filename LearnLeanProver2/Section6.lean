7a424358f144c2bae56b20f44a913174e233e204
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-08-11 04:15:45 +0900

    wip: finish section 6

--- LearnLeanProver2/Section6.lean
+++ LearnLeanProver2/Section6.lean
@@ -136,3 +136,131 @@ variable (euclr : euclidean r)
 
 #check @euclr
 #check euclr
+
+variable (m n : Nat)
+variable (i j : Int)
+
+#check i + m
+#check i + m + j
+#check i + m + n
+
+#check Eq
+#check @Eq
+#check Eq.symm
+#check @Eq.symm
+
+#print Eq.symm
+
+#check And
+#check And.intro
+#check @And.intro
+
+def foo {α : Type u} (x : α) : α := x
+
+#check foo
+#check @foo
+#print foo
+
+#print propext
+
+namespace universe_example
+universe u v w
+def compose {α : Type u} {β : Type v} {γ : Type w}
+    (g :β → γ) (f : α → β) (x : α) : γ :=
+  g (f x)
+end universe_example
+
+def compose2.{u, v, w}
+  {α : Type u} {β : Type v} {γ : Type w}
+    (g :β → γ) (f : α → β) (x : α) : γ :=
+  g (f x)
+
+def compose3 (g : β → γ) (f : α → β)(x : α) : γ :=
+  g (f x)
+
+#check @compose3
+
+namespace ex2
+def id1 : {α : Type} → α → α :=
+  fun x => x
+
+def listId : List ({α : Type} → α → α) :=
+  (fun x => x) :: []
+
+def id2 : {α : Type} → α → α :=
+  @fun α (x : α) => id1 x
+
+def id3 : {α : Type} → α → α :=
+  @fun α x => id1 x
+
+def id4 : {α : Type} → α → α :=
+  fun x => id1 x
+
+def id5 : {α : Type} → α → α :=
+  fun {α} x => id1 x
+end ex2
+
+#check (· + 1)
+#check (2 - ·)
+#eval [1, 2, 3, 4, 5].foldl (·*·) 1
+def f (x y z : Nat) :=
+  x + y + z
+#check (f · 1 ·)
+#eval [(1, 2), (3, 4), (5, 6)].map (·.1)
+
+#check (Prod.mk · (· + 1))
+
+def sum (xs : List Nat) :=
+  xs.foldl (init := 0) (· + ·)
+
+#eval sum [1, 2, 3, 4, 5]
+
+example {a b : Nat} {p : Nat → Nat → Nat → Prop} (h₁ : p a b b) (h₂ : b = a)
+  : p a a b :=
+  Eq.subst (motive := fun x => p a x b) h₂ h₁
+
+example {a b : Nat} {p : Nat → Nat → Nat → Prop} (h₁ : p a b b) (h₂ : b = a)
+  : p a a b :=
+  Eq.subst (motive := fun x => p a x b) h₂ h₁
+
+def ff (x : Nat) (y : Nat := 1) (w : Nat := 2) (z : Nat) :=
+  x + y + w - z
+
+example (x z : Nat) : ff (z := z) x = x + 1 + 2 - z := rfl
+example (x z : Nat) : ff x (z := z) = x + 1 + 2 - z := rfl
+example (x y : Nat) :ff x y = fun z => x + y + 2 - z := rfl
+example : ff = (fun x z => x + 3 - z) := rfl
+example (x : Nat) : ff x = fun z => x + 3 - z := rfl
+example (y : Nat) : ff (y := 5) = fun x z => x + 5 + 2 - z := rfl
+def g {α} [Add α] (a : α) (b? : Option α := none) (c : α) : α :=
+  match b? with
+  | none => a + c
+  | some b => a + b + c
+
+variable {α} [Add α]
+
+example : g = fun (a c : α) => a + c := rfl
+example (x : α) : g (c := x) = fun (a : α) => a + x := rfl
+example (x : α) : g (b? := some x) = fun (a c :α) => a + x + c := rfl
+example (x : α) : g x = fun (c : α) => x + c := rfl
+example (x y : α) : g x y = fun (c : α) => x + y + c := rfl
+
+inductive Term where
+  | var (name : String)
+  | num (val : Nat)
+  | app (fn : Term) (arg : Term)
+  | lambda (name : String) (type : Term) (body : Term)
+
+def getBinderName : Term → Option String
+  | Term.lambda (name := n) .. => some n
+  | _ => none
+
+def getBinderType : Term → Option Term
+  | Term.lambda (type := t) .. => some t
+  | _ => none
+
+example (f : Nat → Nat) (a b c : Nat) : f (a + b + c) = f (a + (b + c)) :=
+  congrArg f (Nat.add_assoc _ _ _)
+
+example (f : Nat → Nat) (a b c : Nat) : f (a + b + c) = f (a + (b + c)) :=
+  congrArg f (Nat.add_assoc ..)
