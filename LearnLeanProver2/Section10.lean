b0034ea0c82a136beb0460e80fbcc53523326dfb
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-08-14 19:08:38 +0900

    add: Section10.lean

---
+++ LearnLeanProver2/Section10.lean
@@ -0,0 +1,25 @@
+structure MyAdd (a : Type) where
+  add : a → a → a
+
+#check @Add.add
+
+def double (s : MyAdd a) (x : a) : a :=
+  s.add x x
+
+#eval double { add := Nat.add } 10
+#eval double { add := Nat.mul } 10
+#eval double { add := Int.mul } 10
+
+class MyAdd2 (a : Type) where
+  add : a → a → a
+
+#check @MyAdd2.add
+
+instance : MyAdd2 Nat where
+  add := Nat.add
+
+instance : MyAdd2 Int where
+  add := Int.add
+
+instance : MyAdd2 Float where
+  add := Float.add
