dc5ca24c2d165e738fb39bf4f27c7ecd8930ae14
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-02-07 20:28:26 +0900

    wip: wip

--- LearnLeanProver2/InteractCommand.lean
+++ LearnLeanProver2/InteractCommand.lean
@@ -1,3 +1,25 @@
+import Lean.Elab.Command
+
 #check_failure 1 + "hello"
 #check_failure (by rfl : 1 = 2)
 #check_failure (by contradiction : 1 + 4 = 5)
+
+#check 'a'
+#check "hello"
+#check 1
+#check 1.0
+#check -1
+#check (1: Int)
+#check [1, 2, 4]
+#check #[1, 2, 3]
+#check fun x => x + 42
+#check true
+#check True
+
+#eval 1 + 1
+
+def fac : Nat → Nat
+  | 0 => 1
+  | n + 1 => (n + 1) * fac n
+
+#eval fac 5
